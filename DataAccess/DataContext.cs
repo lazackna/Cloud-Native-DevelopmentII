b54815d8fe744413f60e5cb0b20fd9b940c338a5
Lazackna <jasperbayens@outlook.com>
2024-06-07 15:50:57 +0200

    Attempt to fix docker

--- DataAccess/DataContext.cs
+++ DataAccess/DataContext.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using DataAccess.Configuration;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection.Emit;
+
 
 namespace DataAccess
 {
     public abstract class DataContext : DbContext
     {
+        public DbSet<Weather> WeatherData { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new WeatherConfiguration());
+        }
 
     }
+
 }
